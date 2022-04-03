054d7430832c15f7c177a08557b46b741daf2a6f
Michael Mathu <mathumichael17@gmail.com>
2022-04-03 09:45:21 +0300

    'refresh'

---
+++ TutorialWithBradTrversy/Program.cs
@@ -0,0 +1,15 @@
+﻿//Namespace
+namespace NumberGuesser
+{
+    //Main Class
+    class Program
+    {
+        //Entry Point Method
+        static void Main(string[] args)
+        {
+            string name = "Mike Mathu";
+
+            Console.WriteLine("Hello " +name);
+        }
+    }
+}
